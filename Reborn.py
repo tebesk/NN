ea336d8d44b68b5c8885d2817ed70e9541e7b880
tebesk <lord.of.the.red@gmail.com>
2016-07-12 14:34:32 +0900

    115行目がおかしい様子。すべて同じ数字が入る
    I tensorflow/stream_executor/dso_loader.cc:108] successfully opened CUDA library libcublas.so locally
    I tensorflow/stream_executor/dso_loader.cc:108] successfully opened CUDA library libcudnn.so locally
    I tensorflow/stream_executor/dso_loader.cc:108] successfully opened CUDA library libcufft.so locally
    I tensorflow/stream_executor/dso_loader.cc:108] successfully opened CUDA library libcuda.so locally
    I tensorflow/stream_executor/dso_loader.cc:108] successfully opened CUDA library libcurand.so locally
    I tensorflow/stream_executor/cuda/cuda_gpu_executor.cc:924] successful NUMA node read from SysFS had negative value (-1), but there must be at least one NUMA node, so returning NUMA node zero
    I tensorflow/core/common_runtime/gpu/gpu_init.cc:102] Found device 0 with properties: 
    name: Quadro M4000
    major: 5 minor: 2 memoryClockRate (GHz) 0.7725
    pciBusID 0000:02:00.0
    Total memory: 8.00GiB
    Free memory: 7.53GiB
    I tensorflow/core/common_runtime/gpu/gpu_init.cc:126] DMA: 0 
    I tensorflow/core/common_runtime/gpu/gpu_init.cc:136] 0:   Y 
    I tensorflow/core/common_runtime/gpu/gpu_device.cc:806] Creating TensorFlow device (/gpu:0) -> (device: 0, name: Quadro M4000, pci bus id: 0000:02:00.0)
    rt_4654.bmp
    train_image
    [[ 0.91764706  0.9137255   0.69803923 ...,  0.01568628  0.02352941
       0.02745098]]
    ansimage
    Traceback (most recent call last):
      File "Reborn.py", line 114, in <module>
        print sess.run(logits)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/client/session.py", line 372, in run
        run_metadata_ptr)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/client/session.py", line 636, in _run
        feed_dict_string, options, run_metadata)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/client/session.py", line 708, in _do_run
        target_list, options, run_metadata)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/client/session.py", line 728, in _do_call
        raise type(e)(node_def, op, message)
    tensorflow.python.framework.errors.InvalidArgumentError: You must feed a value for placeholder tensor 'Placeholder' with dtype float
    	 [[Node: Placeholder = Placeholder[dtype=DT_FLOAT, shape=[], _device="/job:localhost/replica:0/task:0/gpu:0"]()]]
    	 [[Node: sigmoid/Sigmoid/_20 = _Recv[client_terminated=false, recv_device="/job:localhost/replica:0/task:0/cpu:0", send_device="/job:localhost/replica:0/task:0/gpu:0", send_device_incarnation=1, tensor_name="edge_68_sigmoid/Sigmoid", tensor_type=DT_FLOAT, _device="/job:localhost/replica:0/task:0/cpu:0"]()]]
    Caused by op u'Placeholder', defined at:
      File "Reborn.py", line 91, in <module>
        images_placeholder = tf.placeholder(tf.float32, shape=(None, IMAGE_PIXELS))
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/ops/array_ops.py", line 895, in placeholder
        name=name)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/ops/gen_array_ops.py", line 1238, in _placeholder
        name=name)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/ops/op_def_library.py", line 704, in apply_op
        op_def=op_def)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/framework/ops.py", line 2260, in create_op
        original_op=self._default_original_op, op_def=op_def)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow/python/framework/ops.py", line 1230, in __init__
        self._traceback = _extract_stack()

--- Reborn.py
+++ Reborn.py
@@ -4,6 +4,10 @@ import os
 import numpy as np
 import cv2
 import time
+import matplotlib.pyplot as plt
+import PIL
+from PIL import Image
+
 
 IMAGE_WIDTH = 300
 IMAGE_HEIGHT = 256
@@ -35,11 +39,11 @@ def inference(images_placeholder, keep_prob):
 		return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 	#iremonoを生成
-	x  = tf.placeholder(tf.float32, [None,256*300], name = "input")
-	y_ = tf.placeholder(tf.float32, [None,256*300], name = "And")
+	#x  = tf.placeholder(tf.float32, [None,256*300], name = "input")
+	#y_ = tf.placeholder(tf.float32, [None,256*300], name = "And")
 	
 	# 画像をリシェイプ 第2引数は画像数(-1は元サイズを保存するように自動計算)、縦x横、チャネル
-	x_image = tf.reshape(x, [-1,256,300,1])
+	x_image = tf.reshape(images_placeholder, [-1,256,300,1])
 
 
 	with tf.name_scope('conv1') as scope:
@@ -75,7 +79,7 @@ def inference(images_placeholder, keep_prob):
 	return y_conv
 
 
-#######################################################answer making
+####################################################### answer making
 ### training data
 train_array = []
 train_image = []
@@ -84,18 +88,8 @@ train_image = []
 train_label_array = []
 train_label = []
 
-### Read answer image
-Ansfiles = os.listdir('train')
-for k in range(2000):
-	img = cv2.imread("train/"+Ansfiles[k])
-	img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-	train_array.append(img.flatten().astype(np.float32)/255.0)
-
-### change to numpy array
-train_image = np.asarray(train_array)
-
-images_placeholder = tf.placeholder("float", shape=(None, IMAGE_PIXELS))
-labels_placeholder = tf.placeholder("float", shape=(None, NUM_CLASSES))
+images_placeholder = tf.placeholder(tf.float32, shape=(None, IMAGE_PIXELS))
+labels_placeholder = tf.placeholder(tf.float32, shape=(None, NUM_CLASSES))
 keep_prob = tf.placeholder(tf.float32)
 
 logits = inference(images_placeholder, keep_prob)
@@ -105,7 +99,45 @@ saver = tf.train.Saver()
 sess.run(tf.initialize_all_variables())
 saver.restore(sess, "model.ckpt")
 
+### Read answer image
+Ansfiles = os.listdir('nntrn')
+for k in range(1):
+	img = cv2.imread("nntrn/"+Ansfiles[k])
+	img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+	train_array.append(img.flatten().astype(np.float32)/255.0)
+	print Ansfiles[k]
+	### change to numpy array
+	train_image = np.asarray(train_array)
+	print "train_image"
+	print train_image
+	print "ansimage"
+	
+	print sess.run(logits.eval(feed_dict={images_placeholder: train_image, keep_prob: 1.0 }))
+	pred = tf.sigmoid(logits.eval(feed_dict={images_placeholder: train_image,keep_prob: 1.0 })[0])
+	print sess.run(pred)
+	
+	divide = tf.constant([0.7])
+	tf_floordiv=tf.floordiv(pred, divide)
+	print "tf.floordiv"
+	print tf_floordiv
+
+	
+	ansimage = np.asarray(tf_floordiv.eval())
+	print ansimage
+	
+	np_img=np.reshape(ansimage, (256,300))
+	np_256= np_img*256
+	print np_256
+	cv2.imwrite("justtest/"+Ansfiles[k], np_256)
+	
+	
+'''
 for i in range(len(train_image)):
+	print train_image[i]
 	pred = tf.sigmoid(logits.eval(feed_dict={images_placeholder: [train_image[i]],keep_prob: 1.0 })[0])
-	print pred#とりあえず出力
+	#print pred#とりあえず出力
+	train_image = np.asarray(pred.eval())
+	print "answer output"
+	print train_image
+'''
 
