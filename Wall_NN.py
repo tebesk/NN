99a9e7b801e56ac4eadfa5962a715958a515950c
tebesk <lord.of.the.red@gmail.com>
2016-07-12 19:40:29 +0900

    実験：BatchNormalizationなし
    http://qiita.com/rezoolab/items/5cc96b6d31153e0c86bc
    によりBatchNormalizationの重要性が指摘されている。
    そのため、BatchNormalizationの有無の比較を行う。
    
    本コードは、BatchNormalizationがない場合のコードである。

--- Wall_NN.py
+++ Wall_NN.py
@@ -4,21 +4,27 @@ import os
 import numpy as np
 import cv2
 import time
-from matplotlib import pyplot as plt
+
+#import input_data
 
 # training data
+train_array = []
 train_image = []
 # training data label
+train_label_array = []
 train_label = []
 
-# Read answer image
-Ansfiles = os.listdir('160412122017_Ans')
-Ansfiles.sort()
+### Read answer image
+Ansfiles = os.listdir('ans')
 
-## read input image
-#Testfiles = os.listdir('160412122017_def/RT')
-#Testfiles.sort()
+sess= tf.InteractiveSession()
 
+
+
+IMAGE_WIDTH = 300
+IMAGE_HEIGHT = 256
+IMAGE_PIXELS = IMAGE_WIDTH*IMAGE_HEIGHT
+NUM_CLASSES = IMAGE_WIDTH*IMAGE_HEIGHT
 
 #####################################################net
 # 重みを標準偏差0.1の正規分布で初期化
@@ -48,122 +54,151 @@ def max_pool_4x128(x):
     return tf.nn.max_pool(x, ksize=[1, 4, 1, 1],strides=[1, 4, 1, 1], padding='VALID')
 
 #グラフを生成
+x  = tf.placeholder(tf.float32, [None,IMAGE_PIXELS], name = "input")
+y_ = tf.placeholder(tf.float32, [None,IMAGE_PIXELS], name = "And")
 
-x  = tf.placeholder(tf.float32, shape=[None, 512*600],name = "input")
-y_ = tf.placeholder(tf.float32, shape=[None, 512*600], name = "And")
+#正規化処理（以下を参考）
+#http://qiita.com/rezoolab/items/5cc96b6d31153e0c86bc
+#http://qiita.com/sergeant-wizard/items/052c98c6e712a4a8df6a
+def batch_normalization(shape, input):
+  eps = 1e-5
+  gamma = weight_variable([shape])
+  beta = weight_variable([shape])
+  mean, variance = tf.nn.moments(input, [0])
+  return gamma * (input - mean) / tf.sqrt(variance + eps) + beta
 
-sess= tf.InteractiveSession()
 
 ### 1層目 畳み込み層
-
+print "before 1st layer"
+#the first two dimensions are the patch size, 
+#the next is the number of input channels, 
+#and the last is the number of output channels. 
 #[filter_height , filter_width , in_channels, output_channels]
 # 5x5フィルタで32チャネルを出力（入力は白黒画像なので1チャンネル）
-W_conv1 = weight_variable([5,5,1,32])
-# 畳み込み層のバイアス
-b_conv1 = bias_variable([32])
-# 画像をリシェイプ 第2引数は画像数(-1は元サイズを保存するように自動計算)、縦x横、チャネル
-x_image = tf.reshape(x, [-1,512,600,1])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) )
 
+# 画像をリシェイプ 第2引数は画像数(-1は元サイズを保存するように自動計算)、縦x横、チャネル
+x_image = tf.reshape(x, [-1,256,300,1])
+
+with tf.name_scope('conv1') as scope:
+	W_conv1 = weight_variable([5, 5, 1, 32])
+	b_conv1 = bias_variable([32])
+	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+
+print "before 2nd layer"
 ### 2層目 プーリング層
 # 2x2のマックスプーリング層を構築
-h_pool1 = max_pool_2x2(h_conv1)
+with tf.name_scope('pool1') as scope:
+	h_pool1 = max_pool_2x2(h_conv1)
 
 ### 3層目 畳み込み層
-W_conv2 = weight_variable([5,5,32,32])
-b_conv2 = bias_variable([32])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2)+b_conv2 )
+with tf.name_scope('conv2') as scope:
+	W_conv2 = weight_variable([5, 5, 32, 32])
+	b_conv2 = bias_variable([32])
+	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
 ### 4層目 プーリング層
-h_pool2 = max_pool_2x2(h_conv2)
+with tf.name_scope('pool2') as scope:
+	h_pool2 = max_pool_2x2(h_conv2)
 
+print "before 5th layer"
 ### 5層目 全結合層
-# オリジナル画像が512x600で、今回畳み込みでpadding='SAME'を指定しているため
+# オリジナル画像が256x300で、今回畳み込みでpadding='SAME'を指定しているため
 # プーリングでのみ画像サイズが変わる。2x2プーリングで2x2でストライドも2x2なので
-# 縦横ともに各層で半減する。そのため、600or512 / 2 / 2 = 150or128が現在の画像サイズ
+# 縦横ともに各層で半減する。そのため、300or256 / 2 / 2 = 75or64が現在の画像サイズ
 # 全結合層にするために、1階テンソルに変形。画像サイズ縦と画像サイズ横とチャネル数の積の次元
 # 出力は1024（この辺は決めです）　
-W_fc1 = weight_variable([150*128*32,1024*2])
-b_fc1 = bias_variable([1024*2])
+with tf.name_scope('fc1') as scope:
+	W_fc1 = weight_variable([75*64*32, 1024])
+	b_fc1 = bias_variable([1024])
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 75*64*32])
+	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+	#Dropout
+	keep_prob = tf.placeholder(tf.float32)
+	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-h_pool2_flat = tf.reshape(h_pool2,[-1,150*128*32])
-h_fc1= tf.sigmoid(tf.matmul(h_pool2_flat, W_fc1)+b_fc1 )
-
-#Dropout
-#dropout率を入れるための仮のTensor
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
+print "befor readout"
 #Readout Layer
-W_fc2 = weight_variable([1024*2,512*600])
-b_fc2 = bias_variable([512*600])
-#y_conv =tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2)+b_fc2)#結論:Sigmoid is better?
-y_conv =tf.sigmoid(tf.matmul(h_fc1_drop, W_fc2)+b_fc2)
-
-
-#Train and Evaluate the Model
-with tf.name_scope("difference") as scope:
-	difference  = tf.reduce_sum(tf.square(y_ - y_conv))
-	#クロスエントロピーを出力
-	ce_summ = tf.scalar_summary("difference", difference)
+with tf.name_scope('fc2') as scope:
+	W_fc2 = weight_variable([1024, NUM_CLASSES])
+	b_fc2 = bias_variable([NUM_CLASSES])
 	
-with tf.name_scope("train") as scope:
-	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(difference)
-#correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)): meanは値が微妙なので却下.誤差を308*308で割ってる？
+with tf.name_scope('RELU') as scope:
+	y_conv =tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2)+b_fc2)
 
+#Train and Evaluate the Model############################################################
 
+# 評価系の関数を用意
+cross_entropy = -tf.reduce_sum(tf.square(y_ - y_conv))#-tf.reduce_sum(y_*tf.log(y_conv))←うまく行かず
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
+divide = tf.constant([0.7])
+correct_prediction = tf.equal(tf.floordiv(y_conv, divide), y_ )#01に修正divideでわって切り捨て
+accuracy = tf.reduce_sum(tf.cast(correct_prediction, tf.float32), name = "accuracy")
+sess.run(tf.initialize_all_variables())
+
+#tensorboard 書き出し用
+tf.scalar_summary("accuracy" , accuracy)
+
+print "befor merge all"
 # 全ての要約をマージしてそれらを /tmp/mnist_logs に書き出します。
 merged = tf.merge_all_summaries()
 writer = tf.train.SummaryWriter("/tmp/logs",sess.graph)
-tf.initialize_all_variables().run()#sess.run(tf.initialize_all_variables())
+
+
+
 
 print "NetWorkMaking Fin"
-
 #実際の計算　一個ずつファイルを読んでいくことにする
 st = time.time()
 
-for i in range(2):
-	difference = 0
-	summary_str= 0
-	for k in range(900):
-	#if i%2 == 0:
-		#img read(default image)
-		img = cv2.imread("160412122017_Ans/"+Ansfiles[k])
-		img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-		#change to 1 line 
-		img = img.flatten().astype(np.float32)/255.0
-		train_label.append(img)
-		
-		#img read(ans image)
-		if  os.path.isfile("160412122017_def"+Ansfiles[k]):#because Ansfiles are default images(not ans image)
-			def_img = cv2.imread("160412122017_def/"+Ansfiles[k])
-			def_img = cv2.cvtColor(def_img, cv2.COLOR_RGB2GRAY)
-			#change to 1 line 
-			def_img = def_img.flatten().astype(np.float32)/255.0
-			train_image.append(def_img)
-		else:
-			continue
+#画像を配列の中にぶっこんでいく！
+for k in range(2000):
+	#img read(A)
+	img = cv2.imread("ans/"+Ansfiles[k])
+	img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+				 
+	#img read(T)
+	if  os.path.isfile("nntrn/"+Ansfiles[k]):#because Ansfiles are default images(not ans image)
+		def_img = cv2.imread("nntrn/"+Ansfiles[k])
+		def_img = cv2.cvtColor(def_img, cv2.COLOR_RGB2GRAY)			
 
-		# change to numpy array
-		train_image = np.asarray(train_image)
-		train_label = np.asarray(train_label)
+		#we put all image in train array
+		train_label_array.append(img.flatten().astype(np.float32))
+		train_array.append(def_img.flatten().astype(np.float32))
+	else:
+		print Ansfiles[k]
+		continue
 
-		#data input in NN
-		feed = {x:train_image, y_: train_label, keep_prob: 1.0}
-		#calculate 
-		result=sess.run([merged,difference], feed_dict=feed)
-		summary_str+= result[0]
-		difference += result[1]
-		print "***short step time %f [s]" % (time.time() - st)
-	writer.add_summary(summary_str,i)
-	print("difference at step %s : %s"%(i,difference))
-	print "***elapsed time %f [s]" % (time.time() - st)
-	train_step.run(feed_dict={x: train_image, y_: train_label, keep_prob: 1.0})
-print("test accuracy %g"%difference.eval(feed_dict={x:train_image, y_: train_label, keep_prob: 1.0}))
+# change to numpy array
+train_image = np.asarray(train_array)
+train_label = np.asarray(train_label_array)
+print train_image.shape
+print train_label.shape
+
+
+
+
+
+# Let's train 
+for i in range(200):
+	
+	for k in range(50):
+		raw_data = train_image[30*k:30*k+30,:]
+		ans_data = train_label[30*k:30*k+30,:]
+		#print raw_data.shape
+		#print ans_data.shape
+		##本当の学習
+		train_step.run(feed_dict={x:raw_data, y_: ans_data, keep_prob: 0.1})
+		if k==49:
+			##Evaluating Our Model
+			train_accuracy = accuracy.eval({x:raw_data, y_: ans_data, keep_prob: 0.1})
+			summary_str= sess.run(merged, feed_dict={x:raw_data, y_: ans_data, keep_prob: 0.1})
+			print("step %d, training accuracy %g"%(i, train_accuracy))
+			print "***elapsed time %f [s]" % (time.time() - st)
+			writer.add_summary(summary_str,i)
+
 
 #結果の保存
 saver = tf.train.Saver()
-save_path= saver.save(sess,"/home/terumo/py/model.ckpt")
+save_path= saver.save(sess,"/home/ys/Undeux/model.ckpt")
 print("model saved")
